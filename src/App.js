65af46183dca140cd2de8af7a37660c9a8c8e252
Tumpsq <tuhautala@gmail.com>
2020-03-23 08:47:25 +0200

    Content item updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import profile_image from "./assets/img/profile_image.jpg";
+
 import ALEntisointi_cover from "./assets/img/A.L.Entisointi_cover.JPG";
 import MatchPairCardGame_cover from "./assets/img/MatchPairCardGame_cover.JPG";
+
 import { LocationOn, LocalPhone, Email } from "@material-ui/icons";
+
+import { ReactComponent as NetlifyIcon } from "./assets/icon/netlify.svg";
+import { ReactComponent as GithubIcon } from "./assets/icon/github.svg";
 
 import ContactItem from "./components/ContactItem";
 import ContentItem from "./components/ContentItem";
@@ -11,7 +16,7 @@ import ContentItem from "./components/ContentItem";
 function App() {
   const portfolioTitle = "< TUOMO HAUTALA />";
   const portfolioSubTitle = "< PORTFOLIO />";
-  const address = "Hultintie 192 21420 Lieto";
+  const gitHubAddress = "https://github.com/Tumpsq";
   const phoneNumber = "+358 44 301 1879";
   const emailAddress = "tuhautala@gmail.com";
 
@@ -30,45 +35,62 @@ function App() {
       <div className="Portfolio-content">
         <ContentItem
           title="A.L Entisöinti"
-          description="Websites for restoration firm"
+          description="Website for restoration firm"
           screenshot={ALEntisointi_cover}
-          hostLink="https://alentisointi.netlify.com/"
-          codeLink=""
+          netlifyUrl="https://alentisointi.netlify.com/"
+          gitHubUrl={null}
         />
 
         <ContentItem
-          title="Match Pair Game"
+          title="Match Pairs Game"
           description="Game to imporove your memory by finding card pairs"
           screenshot={MatchPairCardGame_cover}
-          hostLink="https://matchpairsgame.netlify.com/"
+          netlifyUrl="https://matchpairsgame.netlify.com/"
+          gitHubUrl="https://github.com/Tumpsq/matchpairsgame"
         />
 
-        <ContentItem
+        {/* <ContentItem
           title="Teams app"
-          description="Practice app for players. Admin users can create events and modify player stats and normal users can attend to them."
+          description="App for managing floorball games"
           screenshot={ALEntisointi_cover}
-          hostLink="https://teamsapp.netlify.com/"
+          netlifyUrl="https://teamsapp.netlify.com/"
+          gitHubUrl={"https://github.com/Tumpsq/teamsapp"}
         />
 
         <ContentItem
           title="Web events"
           description="Websites for restoration firm"
           screenshot=""
-          hostLink="https://eventanimations.netlify.com/"
-        />
+          netlifyUrl="https://eventanimations.netlify.com/"
+          gitHubUrl={null}
+        /> */}
 
         {/* <div className="Portfolio-content-item">1</div>
         <div className="Portfolio-content-item">2</div>
         <div className="Portfolio-content-item">3</div> */}
       </div>
       <div className="Portfolio-contact-infos">
-        <ContactItem icon={<LocationOn />} title="Osoite" value={address} />
-        <ContactItem
-          icon={<LocalPhone />}
-          title="Puhelin"
-          value={phoneNumber}
-        />
-        <ContactItem icon={<Email />} title="Email" value={emailAddress} />
+        <a
+          href="https://github.com/Tumpsq"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <ContactItem
+            icon={<GithubIcon />}
+            title="GitHub"
+            value={gitHubAddress}
+          />
+        </a>
+        <a href="tel:+358-44-301-1879">
+          <ContactItem
+            icon={<LocalPhone />}
+            title="Phone"
+            value={phoneNumber}
+          />
+        </a>
+        <a href="mailto:tuhautala@gmail.com">
+          <ContactItem icon={<Email />} title="Email" value={emailAddress} />
+        </a>
       </div>
     </div>
   );
--- src/components/ContentItem.jsx
+++ src/components/ContentItem.jsx
@@ -1,23 +1,47 @@
 import React from "react";
+import { ReactComponent as NetlifyIcon } from "../assets/icon/netlify.svg";
+import { ReactComponent as GithubIcon } from "../assets/icon/github.svg";
 
-const ContentItem = ({ title, description, screenshot, hostLink }) => {
+const ContentItem = ({
+  title,
+  description,
+  screenshot,
+  netlifyUrl,
+  gitHubUrl
+}) => {
   return (
-    <div
-      className="Content-item"
-      style={{
-        backgroundImage: `url(${screenshot})`
-      }}
-    >
+    <div className="Content-item">
+      <div
+        className="Content-item-background"
+        style={{
+          backgroundImage: `url(${screenshot})`
+        }}
+      />
       <div className="Content-item-content">
         <div className="Content-item-title">
           <h2>{title}</h2>
         </div>
         <div className="Content-item-description">
-          <h2>{description}</h2>
+          <h4>{description}</h4>
         </div>
-        <div className="Content-item-description">
-          <h2>{hostLink}</h2>
-        </div>
+      </div>
+      <div className="Content-item-links">
+        {netlifyUrl && (
+          <a href={netlifyUrl} target="_blank" rel="noopener noreferrer">
+            <div className="Content-item-link">
+              <NetlifyIcon />
+              <h2>Netflify</h2>
+            </div>
+          </a>
+        )}
+        {gitHubUrl && (
+          <a href={gitHubUrl} target="_blank" rel="noopener noreferrer">
+            <div className="Content-item-link">
+              <GithubIcon />
+              <h2>Github</h2>
+            </div>
+          </a>
+        )}
       </div>
     </div>
   );
